83f20a2f04649627723c9cd61a52d229d1d15047
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2021-10-19 07:45:57 -0300

    cleaning vars

--- apigateway/infra/variable.tf
+++ apigateway/infra/variable.tf
@@ -1,15 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-
-variable "vpc_id" {
-  default = "vpc-08ccd18714a2e8437"
-}
-
-variable "subnets" {
-  default = ["subnet-01459f2806e7d9f24", "subnet-080509807e667e94e"]
-}
-
-variable "task_image" {
-  default = "779882487479.dkr.ecr.us-east-1.amazonaws.com/test-secrets:latest"
-}
